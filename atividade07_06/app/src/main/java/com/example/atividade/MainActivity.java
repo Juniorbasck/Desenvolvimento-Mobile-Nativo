7935139875f60627515f07651c426d240cdacb6b
Juniorbasck <adilson.juniorcomunicacao@gmail.com>
2024-06-21 12:04:00 -0300

    atualizacoes

--- atividade07_06/app/src/main/java/com/example/atividade/MainActivity.java
+++ atividade07_06/app/src/main/java/com/example/atividade/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.atividade;
 
+import android.content.Intent; // Adicione essa importação para o Intent
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -19,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button button7 = findViewById(R.id.button7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Segunda.class);
+                startActivity(intent);
+            }
         });
     }
 }
